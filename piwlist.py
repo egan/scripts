98eccaff4a6aac5aaab92993559ca9317fbc80c8
Egan McComb <egan.mccomb@gmail.com>
2011-07-05 23:12:53 -0700

    Translated to Python 3, added nonpretty ouput option.

--- piwlist.py
+++ piwlist.py
@@ -1,18 +1,19 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 ##
 # piwlist.py -- parses output of iwlist scan into table
 #
-# version -- 0.1
+# usage -- sudo iwlist wlan0 scan | piwlist.py [-n|--nopretty]
 #
-# usage -- sudo iwlist wlan0 scan | piwlist.py 
+# notes -- originally written in python v2.x
 #
-# written -- 17 January, 2010 by Hugo Chargois 
+# written -- 17 January, 2010 by Hugo Chargois
 #
-# revised -- 5 July, 2010 by Egan McComb
+# revised -- 5 July, 2011 by Egan McComb
 ##
 
 import sys
+import getopt
 
 ##
 # Functions to parse the properties of each AP (cell).
@@ -24,25 +25,27 @@ def get_name(cell):
 	return matching_line(cell,"ESSID:")[1:-1]
 
 def get_quality(cell):
-	quality = matching_line(cell,"Quality=").split()[0].split('/')
-	return str(int(round(float(quality[0]) / float(quality[1]) * 100))).rjust(3) + " %"
+	quality = matching_line(cell, "Quality=").split()[0].split('/')
+	return str(round(float(quality[0]) / float(quality[1]) * 100)).rjust(3) + "%"
 
 def get_channel(cell):
 	return matching_line(cell,"Channel:")
 
+# XXX: Check WPA version determination efficacy
+#      and WEP identification accuracy.
 def get_encryption(cell):
-	enc=""
+	enc = None
 	if matching_line(cell,"Encryption key:") == "off":
-		enc="Open"
+		enc = "Open"
 	else:
 		for line in cell:
 			matching = match(line,"IE:")
-			if matching!=None:
-				wpa=match(matching,"WPA Version ")
-				if wpa!=None:
-					enc="WPA v."+wpa
-		if enc=="":
-			enc="WEP"
+			if matching != None:
+				wpa = match(matching,"WPA Version ")
+				if wpa != None:
+					enc="WPA v." + wpa
+		if enc == None:
+			enc = "WEP"
 	return enc
 
 def get_address(cell):
@@ -54,11 +57,12 @@ def get_address(cell):
 # Values are obtained in the functions defined above.
 ##
 
-rules={ "Name":get_name,
-	"Quality":get_quality,
-	"Channel":get_channel,
-	"Encryption":get_encryption,
-	"Address":get_address,
+rules = {
+	"Name":	get_name,
+	"Quality":	get_quality,
+	"Channel":	get_channel,
+	"Encryption":	get_encryption,
+	"Address":	get_address,
 	}
 
 ##
@@ -66,75 +70,106 @@ rules={ "Name":get_name,
 ##
 
 def sort_cells(cells):
-	sortby = "Quality"
-	reverse = True
-	cells.sort(None, lambda el:el[sortby], reverse)
+	cells.sort(key=lambda el:el["Quality"], reverse=True)
 
 ##
-# Column definitions using keys using keys from the dictionary.
+# Column definitions using keys from the dictionary.
 ##
 
-columns=["Name","Address","Quality","Channel","Encryption"]
+columns = ["Name","Address","Quality","Channel","Encryption"]
 
 #########
 
 ##
-# Text hacking.
+# Matching functions. 
 ##
 
-def matching_line(lines, keyword):
-	# Returns first matching line in a list of lines. See match():
+def matching_line(lines,keyword):
+	# Returns first matching line in a list of lines. See match().
 	for line in lines:
-		matching=match(line,keyword)
-		if matching!=None:
+		matching = match(line,keyword)
+		if matching != None:
 			return matching
 	return None
 
 def match(line,keyword):
-	# Returns end of line if beginning matches keyword; otherwise return None:
-	line=line.lstrip()
-	length=len(keyword)
+	# Returns end of line if beginning matches keyword; otherwise returns None.
+	line = line.lstrip()
+	length = len(keyword)
 	if line[:length] == keyword:
 		return line[length:]
 	else:
 		return None
 
+##
+# Parsing and printing functions.
+##
+
 def parse_cell(cell):
-	# Applies rules to text describing cell and returns dictionary:
-	parsed_cell={}
+	# Applies rules to text describing cell and returns dictionary.
+	parsed_cell = {}
 	for key in rules:
-		rule=rules[key]
+		rule = rules[key]
 		parsed_cell.update({key:rule(cell)})
 	return parsed_cell
 
 def print_table(table):
-	widths=map(max,map(lambda l:map(len,l),zip(*table))) #functional magic
+	widths = list(map(max,[list(map(len,l)) for  l in zip(*table)]))
 
-	justified_table=[]
+	justified_table = []
 	for line in table:
-		justified_line=[]
-		for i,el in enumerate(line):
+		justified_line = []
+		for i, el in enumerate(line):
 			justified_line.append(el.ljust(widths[i]+2))
 		justified_table.append(justified_line)
 
 	for line in justified_table:
 		for el in line:
-			print el,
-		print
+			print(el,end=" ")
+		print()
 
-def print_cells(cells):
-	table=[columns]
+def print_list(list):
+	for line in list:
+		for el in line:
+			print(el,end="|")
+		print()
+
+def print_nopretty(cells):
+	list = []
 	for cell in cells:
-		cell_properties=[]
+		cell_properties = []
+		for column in columns:
+			cell_properties.append(cell[column])
+		list.append(cell_properties)
+	print_list(list)
+
+def print_pretty(cells):
+	table = [columns]
+	for cell in cells:
+		cell_properties = []
 		for column in columns:
 			cell_properties.append(cell[column])
 		table.append(cell_properties)
 	print_table(table)
 
-def main():
-	# Pretty prints stdin (iwlist scan) into a table:
-	cells=[[]]
-	parsed_cells=[]
+#########
+
+def main(argv):
+
+	try:
+		args, opts = getopt.getopt(argv,"n",["nopretty"])
+		for opt in opts:
+			if opt == "-n":
+				def print_cells(cells): print_nopretty(cells)
+			else:
+				def print_cells(cells): print_pretty(cells)
+	except getopt.error:
+		print("Error: Invalid argument(s)",file=sys.stderr)
+		print("Usage: piwlist [-n|--nopretty]",file=sys.stderr)
+		sys.exit(3)
+
+	cells = [[]]
+	parsed_cells = []
 
 	for line in sys.stdin:
 		cell_line = match(line,"Cell ")
@@ -143,7 +178,7 @@ def main():
 			line = cell_line[-27:]
 		cells[-1].append(line.rstrip())
 
-	cells=cells[1:]
+	cells = cells[1:]
 
 	for cell in cells:
 		parsed_cells.append(parse_cell(cell))
@@ -154,4 +189,4 @@ def main():
 
 #########
 
-main()
+main(sys.argv)
