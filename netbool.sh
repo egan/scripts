b263414e61a3e00e24bf2077018dfbf609f788af
Egan McComb <egan.mccomb@gmail.com>
2011-12-29 19:11:05 -0800

    Initial commit for library.

---
+++ netbool.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##
+# netbool.sh	-- check for proper Internet connectivity
+#
+# usage		-- netbool.sh
+#
+# notes		-- designed for use in other scripts
+#
+# todo		-- more elegant way to do this?
+#
+# written	-- 29 December, 2011 by Egan McComb
+#
+# revised	--
+##
+
+if ping -c 1 example.com &> /dev/null
+then
+	exit 0
+else
+	exit 1
+fi
