27865438b4ee894a6ff5237879e436c2f4d37d09
Egan McComb <egan.mccomb@gmail.com>
2011-12-29 17:52:38 -0800

    Style: better conditional, minor changes

--- xmobar-nettoggle.sh
+++ xmobar-nettoggle.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 ##
-# xmobar-nettoggle.sh -- toggle network interface in xmobar config
+# xmobar-nettoggle.sh	-- toggle active network interface in xmobar
 #
-# usage -- xmobar-nettoggle.sh
+# usage			-- xmobar-nettoggle.sh
 #
-# written -- 19 December, 2011 by Egan McComb
+# notes			-- designed for use from window manager
 #
-# revised --
+# written		-- 19 December, 2011 by Egan McComb
+#
+# revised		--
 ##
 
 xmobarrc="$HOME/.xmobarrc"
 
-if $(grep -q "eth0" < $xmobarrc)
+if grep -q eth0 < $xmobarrc
 then
-	sed -e "s/eth0/wlan0/g" < $xmobarrc | sponge $xmobarrc
+	sed -e "s/eth0/wlan0/g" < $xmobarrc | sponge $xmobarrc || exit 1
 else
-	sed -e "s/wlan0/eth0/g" < $xmobarrc | sponge $xmobarrc
+	sed -e "s/wlan0/eth0/g" < $xmobarrc | sponge $xmobarrc || exit 1
 fi
 
 pkill -x xmobar
